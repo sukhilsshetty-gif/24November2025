b572ecc601c759383791a4c96ac6160b961a106b
sukhi <sukhi@LAPTOP-IND1A1NS>
2026-06-17 07:38:41 +0530

    changes 1 master

---
+++ TestNov2025Batch/src/javaPackage/TestClassone.java
@@ -0,0 +1,11 @@
+package javaPackage;
+
+public class TestClassone {
+
+	public static void main(String[] args) {
+	System.out.println("First Change");
+	System.out.println("Second Change");
+	System.out.println("Third Change");
+	}
+
+}
