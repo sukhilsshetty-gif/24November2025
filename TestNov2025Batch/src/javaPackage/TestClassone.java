5765df49ec45fb5380f6834e79e2aa7203b69b06
Sukhil <sukhil.s.shetty@gmail.com>
2026-06-18 12:59:56 +0530

    commented line

--- TestNov2025Batch/src/javaPackage/TestClassone.java
+++ TestNov2025Batch/src/javaPackage/TestClassone.java
@@ -4,8 +4,8 @@ public class TestClassone {
 
 	public static void main(String[] args) {
 	System.out.println("First Change");
-	System.out.println("Second Change");
-	System.out.println("Third Change");
+	//System.out.println("Second Change");
+	//System.out.println("Third Change");
 
 	}
 
